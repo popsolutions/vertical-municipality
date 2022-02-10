fe616aef82637295a5444d995ec1a99fe75005a1
MateusONunes <mateus.2006@gmail.com>
2022-02-10 18:38:20 -0300

    - Correções em account.invoice -Acrescentado print de progresso do processamento no log

--- property_ga_maintenance/models/invoice.py
+++ property_ga_maintenance/models/invoice.py
@@ -11,18 +11,18 @@ class AccountInvoice(models.Model):
     @api.multi
     def _get_process_methods_list(self):
         methods_list = super(AccountInvoice, self)._get_process_methods_list()
-        methods_list.append('process_property_water_catchment')
+        methods_list.append('process_property_water_consumption')
         return methods_list
 
     @api.multi
-    def process_property_water_catchment(self):
+    def process_property_water_consumption(self):
         """Main method that will be executed by the cron job
         and will create all of invoices from their pending water consumptions"""
-        product_id = self.env.ref('property_water_catchment.product_property_water_catchment')
+        product_id = self.env.ref('property_water_consumption.product_property_water_consumption')
         account_id = product_id.product_tmpl_id.get_product_accounts()[
             'income']
 
-        property_wc_ids = self.env['property.water.catchment'].search(
+        property_wc_ids = self.env['property.water.consumption'].search(
             [('state', 'not in', ['processed'])])
         inv_ids = self.search([('land_id', '!=', False),
                                ('state', 'not in', ['in_payment',
@@ -30,37 +30,30 @@ class AccountInvoice(models.Model):
                                                     'cancel'])])
         inv_land_ids = inv_ids.mapped('land_id').ids
 
-        records_len = len(property_wc_ids)
-        records_index = 0
-
         for p_wc in property_wc_ids:
             if p_wc.land_id.id not in inv_land_ids:
-                self._create_property_wcc_customer_invoice(
+                self._create_property_wc_customer_invoice(
                     p_wc, product_id, account_id)
             else:
                 inv_id = self.search([('land_id', '=', p_wc.land_id.id), ('state', 'not in', ['in_payment',
-                                                                                              'paid',
-                                                                                              'cancel'])], limit=1)
-                inv_id.write({'invoice_line_ids': [(0, 0, {
+                                                    'paid',
+                                                    'cancel'])], limit=1)
+                inv_id.write({'invoice_line_ids': [(0, 0,  { 
                     'product_id': product_id.id,
                     'name': product_id.name,
-                    'price_unit': p_wc.rate_catchment,
+                    'price_unit': p_wc.total,
                     'account_id': account_id.id,
                     'invoice_line_tax_ids': [(6, 0, product_id.taxes_id.ids)],
-                })]})
+                 })]})
             p_wc.state = 'processed'
 
-            records_index += 1
-            print('account.invoice - property.water.catchment - record ' + str(records_index) + '/' + str(records_len))
-
-
     @api.multi
-    def _create_property_wcc_customer_invoice(self, p_wc, product_id, account_id):
+    def _create_property_wc_customer_invoice(self, p_wc, product_id, account_id):
 
         inv_line_vals = {
             'product_id': product_id.id,
             'name': product_id.name,
-            'price_unit': p_wc.rate_catchment,
+            'price_unit': p_wc.total,
             'account_id': account_id.id,
             'invoice_line_tax_ids': [(6, 0, product_id.taxes_id.ids)],
         }
