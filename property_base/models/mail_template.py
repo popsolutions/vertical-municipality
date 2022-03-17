eb1e59c3a637a61879b667a326e90532c04c963e
MateusONunes <mateus.2006@gmail.com>
2022-03-17 20:25:05 -0300

    Ajuste simples - invoice.gera_boleto_pdf()

--- property_base/models/mail_template.py
+++ property_base/models/mail_template.py
@@ -12,7 +12,7 @@ class MailTemplate(models.Model):
         # Anexar pdf do boleto ao e-mail
         for res_id, template in self.web_progress_iter(self.get_email_template(res_ids).items(), 'Gerando boletos'):
             invoice = self.env['account.invoice'].browse(res_id)
-            invoice.get_boleto_pdf()
+            invoice.gera_boleto_pdf()
             attachments = [('Boleto', invoice.file_pdf_id.datas)]
             res[res_id]['attachments'] += attachments
 
