f6ca5151df1ad6e1b0506ab7bb4df5561cc8f2c7
MateusONunes <mateus.2006@gmail.com>
2022-04-18 19:38:39 -0300

    Módulo de impressão de boletos frente e verso em fase inicial.

---
+++ l10n_br_account_payment_brcobranca_batch/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2022 PopSolutions, mateusonunesc@gmail.com
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'L10n Br Account Payment Brcobranca Batch',
+    'summary': """
+        l10n_br_account_payment_brcobranca_batch""",
+    'version': '12.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'PopSolutions, mateusonunesc@gmail.com,Odoo Community Association (OCA)',
+    'website': 'https://www.popsolutions.co',
+    'depends': ['account', 'l10n_br_account_payment_order', 'web'
+    ],
+    'data': [
+        'reports/boleto_verso.xml',
+        'reports/boleto_frente.xml',
+    ],
+    'demo': [
+    ],
+}
