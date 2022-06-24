d6af6301643323ceaa4c73391cc7010d69d1b443
MateusONunes <mateus.2006@gmail.com>
2022-06-24 15:23:50 -0300

    Ajustes nos objetos do banco

--- db/functions/account_invoice_accumulated_create_invoice_id.sql
+++ db/functions/account_invoice_accumulated_create_invoice_id.sql
@@ -3,11 +3,9 @@ CREATE OR REPLACE FUNCTION public.account_invoice_accumulated_create_invoice_id(
  LANGUAGE plpgsql
 AS $function$
   declare anomes int;
-  declare anomes_1 int;
-  declare anomes_2 int;
   declare state varchar;
 begin
-/* versao:2022.05.30
+/* versao:2022.05.31
  *  Esta rotina vai:
    1 - deletar(se exite) registros no account_invoice_line (Referente a <_invoice_id>) referente a boletos atrasados (account_invoice_accumulated_reset)
    2 - Criar em account_invoice_line registros referentes a boleto do mês PASSADO que não foi pago
@@ -15,22 +13,18 @@ begin
 */
 
   select anomes(aci.date_due) anomes,
-         anomes_inc(anomes(aci.date_due), -1) anomes_1,
-         anomes_inc(anomes(aci.date_due), -2) anomes_2,
          aci.state
     from account_invoice aci
    where aci.id = _invoice_id
-    into anomes, anomes_1, anomes_2, state;
+    into anomes, state;
 
   if (state in ('draft', 'open'))  then
     --resetar(deletar) possívels registros acumulados já existentes em account_invoice_line...
     perform account_invoice_accumulated_reset(anomes, _invoice_id);
 
     --Criar registros acumulados(Ou trazer registros atrasados) para account_invoice_line referente ao mes PASSADO (1 mes antes)
-    perform account_invoice_accumulated_create(anomes, anomes_1, _invoice_id);
+    perform account_invoice_accumulated_create(anomes, _invoice_id);
 
-    --Criar registros acumulados(Ou trazer registros atrasados) para account_invoice_line referente ao mes RETRASADO (2 meses antes)
-    perform account_invoice_accumulated_create(anomes, anomes_2, _invoice_id);
   end if;
 
   return true;
