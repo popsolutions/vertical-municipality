329b8937a0ce8cdd3c96a0ba93c8463347a9227e
MateusONunes <mateus.2006@gmail.com>
2022-10-13 11:32:31 -0300

    task:42 Pdf-"Abrir como" (Está sem extensão)

--- property_base/models/mail_template.py
+++ property_base/models/mail_template.py
@@ -13,7 +13,7 @@ class MailTemplate(models.Model):
         # Anexar pdf do boleto ao e-mail
         for res_id, template in self.web_progress_iter(self.get_email_template(res_ids).items(), 'Gerando boletos'):
             ir_attachment = process_boleto_frente_verso(str(res_id), False, True)
-            attachments = [('Boleto', ir_attachment.datas)]
+            attachments = [('Boleto.pdf', ir_attachment.datas)]
             res[res_id]['attachments'] += attachments
 
         return res
