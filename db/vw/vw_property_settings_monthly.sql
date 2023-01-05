6f1c27183fa9caeb3bb3f58868ceff430efee1c2
MateusONunes <mateus.2006@gmail.com>
2023-01-05 15:01:05 -0300

    task:102 - Commit das view APÓS de rodar os scripts

--- db/vw/vw_property_settings_monthly.sql
+++ db/vw/vw_property_settings_monthly.sql
@@ -7,7 +7,21 @@ AS SELECT psm.year_month,
     anomes_inc(psm.year_month, yfi.property_ga_tax) AS year_month_property_ga_tax,
     anomes_inc(psm.year_month, yfi.property_water_consumption) AS year_month_property_water_consumption,
     anomes_inc(psm.year_month, yfi.property_water_catchment) AS year_month_property_water_catchment,
-    psm.nextread_date
-   FROM property_settings_monthly psm,
+    psm.nextread_date,
+    psm.ar_period,
+    psm.ar_ph,
+    psm.ar_ph_limit,
+    psm.ar_uh_color,
+    psm.ar_uh_color_limit,
+    psm.ar_ut_turbidity,
+    psm.ar_ut_turbidity_limit,
+    psm.ar_chlorine_residual,
+    psm.ar_chlorine_residual_limit,
+    psm.ar_fluorides,
+    psm.ar_fluorides_limit,
+    psm.ar_ecoli,
+    psm.ar_ecoli_limit,
+    psm.rate_catchment
+   FROM property_water_catchment_monthly_rate psm,
     vw_product_template_yearmonth_dec_from_invoice yfi
   ORDER BY psm.year_month DESC;
--- db/vw/vw_property_settings_monthly_last.sql
+++ db/vw/vw_property_settings_monthly_last.sql
@@ -7,7 +7,21 @@ AS SELECT psm.year_month,
     psm.year_month_property_ga_tax,
     psm.year_month_property_water_consumption,
     psm.year_month_property_water_catchment,
-    psm.nextread_date
+    psm.nextread_date,
+    rate_catchment,
+    ar_period,
+    ar_ph,
+    ar_ph_limit,
+    ar_uh_color,
+    ar_uh_color_limit,
+    ar_ut_turbidity,
+    ar_ut_turbidity_limit,
+    ar_chlorine_residual,
+    ar_chlorine_residual_limit,
+    ar_fluorides,
+    ar_fluorides_limit,
+    ar_ecoli,
+    ar_ecoli_limit
    FROM vw_property_settings_monthly psm
   ORDER BY psm.year_month DESC
  LIMIT 1;
