cf0a214dac67aaaad6360cf7dc7bf3a216a8fab8
MateusONunes <mateus.2006@gmail.com>
2023-06-28 17:40:04 -0300

    Estava dando erro ao efetuar sql no campo owner_readdate. O Erro estava estourando ao imprimir boleto

--- db/vw/vw_property_water_consumption_unified_group.sql
+++ db/vw/vw_property_water_consumption_unified_group.sql
@@ -1,11 +1,7 @@
 CREATE OR REPLACE VIEW public.vw_property_water_consumption_unified_group
 AS SELECT anomes(v.date::timestamp without time zone) AS anomes,
     v.unified_property_id_orid,
-    string_agg(
-        CASE
-            WHEN v.unified_property_id_orid = v.land_id THEN v.date::character varying
-            ELSE ''::character varying
-        END::text, ''::text)::date AS owner_readdate,
+    min(v.date) filter (where v.unified_property_id_orid = v.land_id) owner_readdate,
     sum(COALESCE(v.last_read, 0)) AS last_read,
     sum(COALESCE(v.current_read, 0)) AS current_read,
     sum(COALESCE(v.consumption, 0)) AS consumption,
