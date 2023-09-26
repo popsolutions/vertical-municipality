2321db8813c42fc415d4aa2cd35fdd4013a21927
MateusONunes <mateus.2006@gmail.com>
2023-09-26 14:22:28 -0300

    Adcionar um progress (web_progress_iter) e para commitar de 20 em 20(indexCommit) no processo de geração de arquivo cnab

--- property_base/models/account_invoice.py
+++ property_base/models/account_invoice.py
@@ -294,3 +294,89 @@ class AccountInvoice(models.Model):
         else:
             return res[0]
 
+    @api.multi
+    def create_account_payment_line(self):
+        # override de /home/mateus/OdooDev/odoo/custom-addons/riviera/bank-payment/account_payment_order/models/account_invoice.py
+        # Override para adcionar um progress (web_progress_iter) e para commitar de 20 em 20(indexCommit)
+        apoo = self.env['account.payment.order']
+        result_payorder_ids = []
+        action_payment_type = 'debit'
+        indexCommit = 0
+
+        for inv in self.web_progress_iter(self):
+            if inv.state != 'open':
+                raise UserError(_(
+                    "The invoice %s is not in Open state") % inv.number)
+            if not inv.move_id:
+                raise UserError(_(
+                    "No Journal Entry on invoice %s") % inv.number)
+            applicable_lines = inv.move_id.line_ids.filtered(
+                lambda x: (
+                    not x.reconciled and x.payment_mode_id.payment_order_ok and
+                    x.account_id.internal_type in ('receivable', 'payable') and
+                    not any(p_state in ('draft', 'open', 'generated')
+                            for p_state in x.payment_line_ids.mapped('state'))
+                )
+            )
+            if not applicable_lines:
+                raise UserError(_(
+                    'No Payment Line created for invoice %s because '
+                    'it already exists or because this invoice is '
+                    'already paid.') % inv.number)
+            payment_modes = applicable_lines.mapped('payment_mode_id')
+            if not payment_modes:
+                raise UserError(_(
+                    "No Payment Mode on invoice %s") % inv.number)
+            for payment_mode in payment_modes:
+                payorder = apoo.search([
+                    ('payment_mode_id', '=', payment_mode.id),
+                    ('state', '=', 'draft')
+                ], limit=1)
+                new_payorder = False
+                if not payorder:
+                    payorder = apoo.create(inv._prepare_new_payment_order(
+                        payment_mode
+                    ))
+                    new_payorder = True
+                result_payorder_ids.append(payorder.id)
+                action_payment_type = payorder.payment_type
+                count = 0
+                for line in applicable_lines.filtered(
+                    lambda x: x.payment_mode_id == payment_mode
+                ):
+                    line.create_payment_line_from_move_line(payorder)
+                    count += 1
+                if new_payorder:
+                    inv.message_post(body=_(
+                        '%d payment lines added to the new draft payment '
+                        'order %s which has been automatically created.')
+                        % (count, payorder.name))
+                else:
+                    inv.message_post(body=_(
+                        '%d payment lines added to the existing draft '
+                        'payment order %s.')
+                        % (count, payorder.name))
+
+            indexCommit += 1
+
+            if indexCommit >= 20:
+                self.env.cr.commit()
+                indexCommit = 0
+
+        action = self.env['ir.actions.act_window'].for_xml_id(
+            'account_payment_order',
+            'account_payment_order_%s_action' % action_payment_type)
+        if len(result_payorder_ids) == 1:
+            action.update({
+                'view_mode': 'form,tree,pivot,graph',
+                'res_id': payorder.id,
+                'views': False,
+                })
+        else:
+            action.update({
+                'view_mode': 'tree,form,pivot,graph',
+                'domain': "[('id', 'in', %s)]" % result_payorder_ids,
+                'views': False,
+                })
+        return action
+
